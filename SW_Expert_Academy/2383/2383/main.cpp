87a98e1e7b24c573285c32ea525b165fbac90615
jwj <ehdwlsdnwls@naver.com>
2019-04-10 09:33:07 +0900

    start to slove 2383

---
+++ SW_Expert_Academy/2383/2383/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdio.h>
+
+#pragma warning (disable : 4996)
+
+using namespace std;
+
+#define MAX 20
+
+int main(int argc, char** arg)
+{
+	int test_case;
+	int T;
+
+	cin >> T;
+
+	for (test_case = 1; test_case <= T; ++test_case)
+	{
+		int map[MAX][MAX] = { 0, };
+
+		
+		printf("#%d %d\n", test_case, answer);
+	}
+	return 0;
+}
