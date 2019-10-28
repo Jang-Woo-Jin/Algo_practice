eb691858c9992379dd1787863c6ec935e68a2489
woojin <ehdwlsdnwls@naver.com>
2019-10-28 01:14:29 -0400

    Solving

--- Purdue/My/1535/1535/main.cpp
+++ Purdue/My/1535/1535/main.cpp
@@ -1,19 +1,39 @@
 #include <stdio.h>
 #include <string.h>
+#include <algorithm>
 #pragma warning (disable : 4996)
 
 int main() {
 	int num;
-	int happy[20], life[20];
-	memset(happy, -1, sizeof(int) * 20);
-	memset(life, -1, sizeof(int) * 20);
+	std::pair<int, int> people[20];
+	memset(people, -1, sizeof(std::pair<int, int>) * 20);
 	
 	scanf("%d", &num);
-	for (int i = 0; i < num; i++) scanf("%d", &life[i]);
-	for (int i = 0; i < num; i++) scanf("%d", &happy[i]);
+	for (int i = 0; i < num; i++) scanf("%d", &people[i].first);
+	for (int i = 0; i < num; i++) scanf("%d", &people[i].second);
 	
-	int maxLife = 100;
-	int happy = 0;
+	sort(people, people + num);
+	
+	int table[101][21];
+	memset(table, -1, sizeof(int) * 101 * 21);
 
+	for (int i = 0; i < 21; i++) table[0][i] = 0;
+	for (int i = 0; i < 101; i++) table[i][0] = 0;
+	
+	for (int i = 1; i <= 100; i++) {
+		for (int j = 1; j <= num; j++) {
+			if (people[j - 1].first <= i) {
+				int origin = table[i][j - 1];
+				int added = table[i - people[j - 1].first][j - 1] + people[j - 1].second;
+				table[i][j] = origin > added ? origin : added;
+
+			}
+			else {
+				table[i][j] = table[i][j - 1];
+			}
+		}
+	}
+	if (num > 0) printf("%d", table[99][num]);
+	else printf("0");
 	return 0;
 }
