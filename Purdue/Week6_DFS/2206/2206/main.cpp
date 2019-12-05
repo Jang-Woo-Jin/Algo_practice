40243d5a949714f9911cb0fa27e66acfdd494c47
woojin <ehdwlsdnwls@naver.com>
2019-12-05 02:21:33 -0500

    fail, need to try with DFS

--- Purdue/Week6_DFS/2206/2206/main.cpp
+++ Purdue/Week6_DFS/2206/2206/main.cpp
@@ -1,35 +1,29 @@
 #include <stdio.h>
+#include <algorithm>
+#include <queue>
+
 #pragma warning (disable : 4996)
+
+using namespace std;
 
 const int dx[4] = { 0, 0, 1, -1 };
 const int dy[4] = { 1, -1, 0, 0 };
 
 bool map[1000][1000] = { false };
-bool visited[1000][1000] = { false };
+int visited[1000][1000][2] = { 0, };
 
-void dfs(int count, int x, int y, int N, int M, bool chance, int &min) {
-	if (x == N -1 && y == M - 1) {
-		if (min > count && count != -1) min = count;
-		return;
+class Point {
+public:
+	int x;
+	int y;
+	int count;
+	Point();
+	Point(int x, int y, int count) {
+		this->x = x;
+		this->y = y;
+		this->count = count;
 	}
-	visited[x][y] = true;
-	for (int i = 0; i < 4; i++) {
-		int nx = x + dx[i], ny = y + dy[i];
-		if (nx > -1 && nx < N && ny > -1 && ny < M) {
-			if (visited[nx][ny]) continue;
-			if (!map[nx][ny]) {
-				visited[nx][ny] = true;
-				dfs(count + 1, nx, ny, N, M, chance, min);
-				visited[nx][ny] = false;
-			}
-			else if(chance) {
-				visited[nx][ny] = true;
-				dfs(count + 1, nx, ny, N, M, !chance, min);
-				visited[nx][ny] = false;
-			}
-		}
-	}
-}
+};
 
 int main() {
 	int N, M;
@@ -43,9 +37,18 @@ int main() {
 			map[i][j] = input - 48;
 		};
 	}
-	int answer = 1000 * 1000 + 1;
-	dfs(1, 0, 0, N, M, true, answer);
-	if (answer == 1000 * 1000 + 1) answer = -1;
-	printf("%d", answer);
+	visited[0][0][0] = 1;
+	visited[N - 1][M - 1][0] = visited[N - 1][M - 1][1] = -1;
+	
+	queue<Point> q;
+	q.push(Point(0, 0, 1));
+	while(!q.empty()){
+		Point current = q.front();
+		q.pop();
+		for (int i = 0; i < 4; i++) {
+
+		}
+	}
+	//printf("%d", visited[N - 1][M - 1][0] < visited[N - 1][M - 1][1] ? visited[N - 1][M - 1][0] : visited[N - 1][M - 1][1]);
 	return 0;
 }
