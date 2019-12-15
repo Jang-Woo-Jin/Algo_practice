b909ab0d961d2db50f3d51ab4e878b0049369584
woojin <ehdwlsdnwls@naver.com>
2019-12-15 17:06:58 -0500

    change logic but fail

--- Purdue/Week9_ShortestPath&BinarySearch/1654/1654/main.cpp
+++ Purdue/Week9_ShortestPath&BinarySearch/1654/1654/main.cpp
@@ -9,30 +9,28 @@ int main() {
 	scanf("%d %d", &line_num, &cut_num);
 
 	int lines[10000] = { 0, };
+	int max_line = 0;
 	for (int i = 0; i < line_num; i++) {
 		scanf("%d", &lines[i]);
+		max_line = max_line > lines[i] ? max_line : lines[i];
 	}
-	sort(lines, lines + line_num);
-
-	bool flag = true;
-	int num = lines[0];
-	int add_num = lines[0];
-	int answer = 0;
-	while (flag) {
-		add_num = (add_num / 2 + add_num % 2);
+	
+	int start = 0, end = max_line;
+	long long int middle = 0;
+	int max = 0;
+	while (1) {
+		middle = (start + end) / 2;
 		int count = 0;
 		for (int i = 0; i < line_num; i++) {
-			count += (lines[i] / num);
+			count += (lines[i] / middle);
 		}
 		if (count >= cut_num) {
-			if (num == answer) break;
-			if (num > answer) answer = num;
-			num += add_num;
+			start = middle + 1;
+			max = max > middle ? max : middle;
 		}
-		else {
-			num -= add_num;
-		}
+		else end = middle - 1;
+		if (middle  == start || middle == end) break;
 	}
-	printf("%d", answer);
+	printf("%d", max);
 	return 0;
 }
